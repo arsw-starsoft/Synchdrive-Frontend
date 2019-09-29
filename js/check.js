86c0e91ef062a8c5e1875b0056a16d93fdb06238
luis572 <luispg572@gmail.com>
2019-09-29 17:47:36 -0500

    correccion

--- js/check.js
+++ js/check.js
@@ -44,6 +44,14 @@ function probar (){
       cont = cont + 1;
      }
     }
-    
+}
+function selectAnos(){
+    var from = new Date(1990,1,1);
+    var to = new Date();
+    for (var day = from; day <= to; day.setFullYear(day.getFullYear() + 1)) {
+        $("#year").append("<option value='" + day.getFullYear() + "'>" + day.getFullYear() + "</option>");
+    }
 
 }
+    
+
